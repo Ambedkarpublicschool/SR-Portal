ba3daf3bd2fbd6c7e606c9c430d2a8b963d6a516
Ambedkarpublicschool <apsmadhopur2024@gmail.com>
2026-09-04 15:08:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyO2vwEo5TJxcXEGDa0flJWjhbkqvQSDXPdToUN4-reC4hYZTYu7gDVOBFMBL_Ahgd1yg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwr4v-gOuZw2E7dKt6klRwpMgWd2x7WDL3qzu6AfKYr0pWqX5isWkqpzUMF7NijFXQdlw/exec";
